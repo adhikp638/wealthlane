f45ef4ad876c469f5294897601e38f809a30476a
adhikp638 <adhikp638@gmail.com>
2023-08-13 08:37:53 -0400

    including the latest customer persona video

--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
-import abc from "../../assets/video/cust_from_davinci.mp4";
+import abc from "../../assets/video/cust_persona.mp4";
 import system_img  from "../../assets/img/system.jpeg";
 
 const Container = styled.div``;
